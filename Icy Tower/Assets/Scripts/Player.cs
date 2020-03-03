94e7be338df843b47aa3eb64a7e2a5ca3e05f731
burakulusahan <burakulusahan@ganover.com>
2020-03-03 23:00:38 +0300

    Move the player

---
+++ Icy Tower/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private float _movementSpeed;
+
+    Rigidbody2D rb;
+
+    private float _movement = 0f;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+
+        _movement = (Input.GetAxis("Horizontal") * _movementSpeed);
+    }
+
+    void FixedUpdate() {
+        Vector2 velocity = rb.velocity;
+        velocity.x = _movement;
+        rb.velocity = velocity;
+    }
+}
