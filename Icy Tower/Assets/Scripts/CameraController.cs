2a41414bc3140ff99c055f9b5bf777d8f0801407
burakulusahan <burakulusahan@ganover.com>
2020-03-04 12:48:09 +0300

    Move the camera when player jumped high

---
+++ Icy Tower/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	[SerializeField]
+	private Transform _target;
+	[SerializeField]
+	private float _offset = 0f;
+
+	void LateUpdate() {
+		if (_target.position.y + _offset > transform.position.y) {
+
+			Vector3 newPos = new Vector3(transform.position.x, _target.position.y + _offset, transform.position.z);
+			transform.position = Vector3.Lerp(transform.position , newPos,10f);
+		}
+	}
+}
