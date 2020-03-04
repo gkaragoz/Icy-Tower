f3df37995b785396377472d1334fdc90a46709a5
burakulusahan <burakulusahan@ganover.com>
2020-03-04 21:04:13 +0300

    Spawn platform objects from pool

---
+++ Icy Tower/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ObjectPooler))]
+public class GameManager : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static GameManager instance;
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjectPooler.instance.InitializePool("Platform");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Icy Tower/Assets/Scripts/Platform.cs
+++ Icy Tower/Assets/Scripts/Platform.cs
@@ -7,7 +7,7 @@ public class Platform : MonoBehaviour {
     [SerializeField]
     private float _jumpForce = 5f;
     [SerializeField]
-    private float _comboJumoForce = 10f;
+    private float _comboJumpForce = 10f;
     [SerializeField]
     private float _comboTrashHold = 0f;
 
@@ -19,7 +19,7 @@ public class Platform : MonoBehaviour {
 
             if (rb != null) {
                 Vector2 velocity = rb.velocity;
-                velocity.y = Mathf.Abs(velocity.x) > _comboTrashHold ? _comboJumoForce : _jumpForce;
+                velocity.y = Mathf.Abs(velocity.x) > _comboTrashHold ? _comboJumpForce : _jumpForce;
                 rb.velocity = velocity;
             }
         }
