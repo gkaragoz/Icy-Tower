503db13de4c4aef7c90d993a5aab67ab655db3fd
burakulusahan <burakulusahan@ganover.com>
2020-03-04 13:01:48 +0300

    Move the camera by time

--- Icy Tower/Assets/Scripts/CameraController.cs
+++ Icy Tower/Assets/Scripts/CameraController.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-	[SerializeField]
-	private Transform _target;
-	[SerializeField]
-	private float _offset = 0f;
+public class CameraController : MonoBehaviour {
+    [SerializeField]
+    private Transform _target = null;
+    [SerializeField]
+    private float _offset = 0f;
+    [SerializeField]
+    private float _cameraSpeed = 0f;
+   
+    void LateUpdate() {
+        if (_target.position.y + _offset > transform.position.y) {
 
-	void LateUpdate() {
-		if (_target.position.y + _offset > transform.position.y) {
+            Vector3 newPos = new Vector3(transform.position.x, _target.position.y + _offset, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPos, 10f);
+        } else {
+            transform.Translate(Vector3.up * Time.deltaTime * _cameraSpeed);
 
-			Vector3 newPos = new Vector3(transform.position.x, _target.position.y + _offset, transform.position.z);
-			transform.position = Vector3.Lerp(transform.position , newPos,10f);
-		}
-	}
+        }
+    }
 }
