6ea844d64ea720d5c7294b7908f5cec1d9480a6e
burakulusahan <burakulusahan@ganover.com>
2020-03-12 22:04:37 +0300

    Import Tileable Bricks Wall

---
+++ Icy Tower/Assets/Scripts/Platform Scripts/Platform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour, IPooledObject {
+
+    
+    public void OnObjectReused() {
+        gameObject.SetActive(true);
+    }
+
+
+
+}
---
+++ Icy Tower/Assets/Scripts/Platform Scripts/PlatformGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    #region Singleton
+
+    public static PlatformGenerator instance;
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    #endregion
+
+    [SerializeField]
+    private float _lastPlatformY = -4f;
+
+    private void Start() {
+        for (int i = 0; i < ObjectPooler.instance.GetGameObjectsOnPool("Platform").Length; i++) {
+            SpawnPlatform();
+        }
+    }
+
+    //private IEnumerator ICheckPlatforms() {
+    //    WaitForSeconds waitForSeconds = new WaitForSeconds(.5f);
+
+    //    while (true) {
+    //        yield return waitForSeconds;
+
+    //        SpawnPlatform();
+    //    }
+    //}
+
+    public void SpawnPlatform() {
+        Vector3 randomPosition = new Vector3(
+            Random.Range(GameManager.instance.LeftPlatformPivot.position.x, GameManager.instance.RightPlatformPivot.position.x),
+              _lastPlatformY += 2f,
+              0);
+        ObjectPooler.instance.SpawnFromPool("Platform", randomPosition, Quaternion.identity);
+
+
+    }
+}
