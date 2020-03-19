abd81d4c6f45dd8dce864fa3664c6d10fa38c763
burakulusahan <burakulusahan@ganover.com>
2020-03-19 20:32:00 +0300

    Spawn Platform from object pool

---
+++ Icy Tower/Assets/Scripts/Manager Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    [SerializeField]
+    private Transform _leftMapSpawnTransform = null;
+    [SerializeField]
+    private Transform _rightMapSpawnTransform = null;
+
+
+
+    public Transform LeftMapSpawnTransform { get { return _leftMapSpawnTransform; } }
+    public Transform RightMapSpawnTransform { get { return _rightMapSpawnTransform; } }
+
+    private void Start() {
+        ObjectPooler.instance.InitializePool("Platform");
+    }
+}
