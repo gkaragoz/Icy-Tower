99a567c77912543ef160385558a391170807a5a6
burakulusahan <burakulusahan@ganover.com>
2020-03-20 16:28:18 +0300

    Scroll the camera based on Player position

---
+++ Icy Tower/Assets/Scripts/Manager Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField]
+    private Transform _target = null;
+    [SerializeField]
+    private float _offset = 0f;
+    [SerializeField]
+    private float _scrollSpeed = 0f;
+    [SerializeField]
+    private float _smothSpeed = 0f;
+
+
+
+    void LateUpdate() {
+        if (_target.position.y + _offset > transform.position.y) {
+            Vector3 newPos = new Vector3(transform.position.x, _target.position.y + _offset, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, newPos, _smothSpeed);
+        } else {
+
+            transform.Translate(Vector3.up * Time.deltaTime * _scrollSpeed);
+        }
+    }
+}
