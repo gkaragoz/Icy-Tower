adf6374a6c118d5c1a9ad95c091c103acd99bb18
burakulusahan <burakulusahan@ganover.com>
2020-03-20 17:00:37 +0300

    Spawn and destroy platforms continuously

---
+++ Icy Tower/Assets/Scripts/Collector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Collector : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Platform") {
+            other.gameObject.SetActive(false);
+            SpawnManager.instance.SpawnPlatform();
+        }
+    }
+}
--- Icy Tower/Assets/Scripts/Manager Scripts/SpawnManager.cs
+++ Icy Tower/Assets/Scripts/Manager Scripts/SpawnManager.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour {
 
+    #region Singleton
+
+    public static SpawnManager instance;
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    #endregion
+
     private float _firstPlatformPosition = 0f;
     private void Start() {
         for (int i = 0; i < ObjectPooler.instance.GetGameObjectsOnPool("Platform").Length; i++) {
@@ -11,7 +23,7 @@ public class SpawnManager : MonoBehaviour {
         }
     }
 
-    private void SpawnPlatform() {
+    public void SpawnPlatform() {
         Vector3 _randomPos = new Vector3(Random.Range(GameManager.instance.LeftMapSpawnTransform.position.x, GameManager.instance.RightMapSpawnTransform.position.x), //x
                                          _firstPlatformPosition += 4f,                                                                                                //y
                                          0f);                                                                                                                         //z
