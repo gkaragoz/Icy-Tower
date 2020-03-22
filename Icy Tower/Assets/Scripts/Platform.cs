9a8e2453964c380afa84515ed13aa5b6a381d878
burakulusahan <burakulusahan@ganover.com>
2020-03-22 21:58:17 +0300

    Randomize platform scale

--- Icy Tower/Assets/Scripts/Platform.cs
+++ Icy Tower/Assets/Scripts/Platform.cs
@@ -1,7 +1,23 @@
 ﻿using UnityEngine;
 
 public class Platform : MonoBehaviour, IPooledObject {
+
+    [SerializeField]
+    private int _maxPlatformScale = 0;
+    [SerializeField]
+    private int _minPlatformScale = 0;
+
+
+
     public void OnObjectReused() {
         gameObject.SetActive(true);
+    }
+
+    public int GetMaxPlatformScale() {
+        return _maxPlatformScale;
+    }
+
+    public int GetMinPlatformScale() {
+        return _minPlatformScale;
     }
 }
