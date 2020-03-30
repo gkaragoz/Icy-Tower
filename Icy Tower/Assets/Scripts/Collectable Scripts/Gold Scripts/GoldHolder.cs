9c048555dc382f578ad56f172d47b220be85168f
burakulusahan <burakulusahan@ganover.com>
2020-03-30 18:06:42 +0300

    Fix gold spawn positions when reused

--- Icy Tower/Assets/Scripts/Collectable Scripts/Gold Scripts/GoldHolder.cs
+++ Icy Tower/Assets/Scripts/Collectable Scripts/Gold Scripts/GoldHolder.cs
@@ -6,6 +6,8 @@ public class GoldHolder : MonoBehaviour, IPooledObject {
 
     [SerializeField]
     private Gold[] _golds = null;
+    private Vector3[] _positions;
+    private bool isFirstSpawn = true;
 
     private void SetGolds() {
         foreach (var gold in _golds) {
@@ -13,8 +15,27 @@ public class GoldHolder : MonoBehaviour, IPooledObject {
         }
     }
 
+    private void SaveGoldPositions() {
+        _positions = new Vector3[_golds.Length];
+        for (int i = 0; i < _golds.Length; i++) {
+            _positions[i] = _golds[i].transform.localPosition;
+        }
+    }
+
+    private void SetGoldPositions() {
+        for (int i = 0; i < _golds.Length; i++) {
+            _golds[i].transform.localPosition = _positions[i];
+        }
+    }
+
     [System.Obsolete]
     public void OnObjectReused() {
+        if (isFirstSpawn) {
+            SaveGoldPositions();
+            isFirstSpawn = false;
+        } else {
+            SetGoldPositions();
+        }
         SetGolds();
         gameObject.SetActiveRecursively(true);
     }
