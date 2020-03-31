47922630ae61875f4a51778920ab445b3b60ae6e
burakulusahan <burakulusahan@ganover.com>
2020-03-31 12:48:14 +0300

    Add Umbrella Scriptable object and stats

---
+++ Icy Tower/Assets/Scripts/Collectable Scripts/Umbrella/UmbrellaStats.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class UmbrellaStats : MonoBehaviour{
+
+
+    [Header("Initialization")]
+    [SerializeField]
+    private Umbrella_SO umbrella_Template = null;
+
+    [Header("Debug")]
+    [SerializeField]
+    [Utils.ReadOnly]
+    private Umbrella_SO _umbrella = null;
+
+    #region Initializations
+
+    private void Awake() {
+        if (umbrella_Template != null) {
+            _umbrella = Instantiate(umbrella_Template);
+        }
+    }
+    #endregion
+
+    #region Setters
+
+    public void SetName(string name) {
+        _umbrella.Name = name;
+    }
+
+    public void SetDuration(float duration) {
+        _umbrella.Duration = duration;
+    }
+
+    public void SetMoveSpeed(float speed) {
+        _umbrella.MoveSpeed = speed;
+    }
+
+    #endregion
+
+    #region Getters
+
+    public string GetName() {
+        return _umbrella.Name;
+    }
+
+    public GameObject GetPrefab() {
+        return _umbrella.Prefab;
+    }
+
+    public float GetDuration() {
+        return _umbrella.Duration;
+    }
+
+    public float GetMoveSpeed() {
+        return _umbrella.MoveSpeed;
+    }
+
+    #endregion
+}
---
+++ Icy Tower/Assets/Scripts/Collectable Scripts/Umbrella/Umbrella_SO.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Umbrella Stats", menuName = "Scriptable Objects/Umbrella Stats")]
+public class Umbrella_SO : ScriptableObject {
+
+    [SerializeField]
+    private string _name = "Umbrella";
+
+    [SerializeField]
+    private GameObject _prefab;
+
+    [SerializeField]
+    private float _duration;
+
+    [SerializeField]
+    private float _moveSpeed;
+
+    public string Name {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public GameObject Prefab {
+        get { return _prefab; }
+        set { _prefab = value; }
+    }
+
+    public float Duration {
+        get { return _duration; }
+        set { _duration = value; }
+    }
+
+    public float MoveSpeed {
+        get { return _moveSpeed; }
+        set { _moveSpeed = value; }
+    }
+}
+
+
