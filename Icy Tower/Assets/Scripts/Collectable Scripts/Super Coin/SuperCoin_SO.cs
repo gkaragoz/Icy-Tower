d336fb9296cb4af6b56a219ada7cd966ee92cf8e
burakulusahan <burakulusahan@ganover.com>
2020-03-31 15:06:03 +0300

    Add Super Coin Scriptable object and stats

---
+++ Icy Tower/Assets/Scripts/Collectable Scripts/Super Coin/SuperCoin_SO.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "SuperCoin Stats", menuName = "Scriptable Objects/SuperCoin Stats")]
+public class SuperCoin_SO : ScriptableObject{
+
+
+    [SerializeField]
+    private string _name = "Super Coin";
+
+    [SerializeField]
+    private GameObject _prefab;
+
+    [SerializeField]
+    private int amount;
+
+    public string Name {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public GameObject Prefab {
+        get { return _prefab; }
+        set { _prefab = value; }
+    }
+
+    public int Amount {
+        get { return amount; }
+        set { amount = value; }
+    }
+}
