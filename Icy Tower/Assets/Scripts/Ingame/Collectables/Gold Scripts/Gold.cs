b7d44b53bfa2bf78d45d7d3e1ec47fd5aedf0d5b
gkaragoz <gkaragoz96@gmail.com>
2020-04-03 11:43:47 +0300

    Refactor Gold.cs script.

--- Icy Tower/Assets/Scripts/Ingame/Collectables/Gold Scripts/Gold.cs
+++ Icy Tower/Assets/Scripts/Ingame/Collectables/Gold Scripts/Gold.cs
@@ -8,7 +8,7 @@ public class Gold : MonoBehaviour {
 
     private float _flySpeed = 0.2f;
     private int _coinScore = 0;
-    private string _goldType = "";
+    private GoldTypes _goldType;
     private bool _hasInteractedWithMagnet = false;
 
     public void CreateGold() {
@@ -26,24 +26,24 @@ public class Gold : MonoBehaviour {
     private void SetGoldType() {
         int enumLenght = Enum.GetNames(typeof(GoldTypes)).Length;
         int _randomGoldType = UnityEngine.Random.Range(0, enumLenght);
-        _goldType = Enum.GetName(typeof(GoldTypes), _randomGoldType);
+        _goldType = (GoldTypes)_randomGoldType;
     }
 
     private void SetGoldValues() {
         switch (_goldType) {
-            case "YellowGold":
+            case GoldTypes.YellowGold:
                 _coinScore = 1;
                 _goldRenderer.material.SetColor("_BaseColor", Color.yellow);
                 break;
-            case "GreenGold":
+            case GoldTypes.GreenGold:
                 _coinScore = 2;
                 _goldRenderer.material.SetColor("_BaseColor", Color.green);
                 break;
-            case "BlueGold":
+            case GoldTypes.BlueGold:
                 _coinScore = 3;
                 _goldRenderer.material.SetColor("_BaseColor", Color.blue);
                 break;
-            case "RedGold":
+            case GoldTypes.RedGold:
                 _coinScore = 5;
                 _goldRenderer.material.SetColor("_BaseColor", Color.red);
                 break;
@@ -52,7 +52,6 @@ public class Gold : MonoBehaviour {
                 break;
         }
     }
-
 
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "FullCollider") {
