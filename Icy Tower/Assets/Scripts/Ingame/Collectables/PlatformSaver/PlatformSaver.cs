21c449c6e4c1d5c6fdb24d6928cfeceb0c35f572
burakulusahan <burakulusahan@ganover.com>
2020-04-04 09:48:55 +0300

    Refactor Platform Saver Power Up

--- Icy Tower/Assets/Scripts/Ingame/Collectables/PlatformSaver/PlatformSaver.cs
+++ Icy Tower/Assets/Scripts/Ingame/Collectables/PlatformSaver/PlatformSaver.cs
@@ -11,39 +11,52 @@ public class PlatformSaver : MonoBehaviour {
     [Utils.ReadOnly]
     [SerializeField]
     private Platform[] _platforms;
-    [Utils.ReadOnly]
-    [SerializeField]
-    private GameState _gameState;
-
 
 
     private void Start() {
         _platformSaverStats = GetComponent<PlatformSaverStats>();
         _platformCountToMaximize = _platformSaverStats.GetPlatformCount();
+        GameManager.instance.OnGameStateChanged += GetPlatforms;
 
     }
 
     private void FixedUpdate() {
-        if (Input.GetKeyDown(KeyCode.M))
+        if (Input.GetKeyDown(KeyCode.M)) {
+            _platformCountToMaximize = _platformSaverStats.GetPlatformCount();
             MaximizePlatformScale();
-
-        _gameState = GameManager.instance.GameStateEnum;
-        if (_gameState == GameState.Gameplay)
-            GetPlatforms();
+        }
     }
 
-    private void GetPlatforms() {
-        _platforms = new Platform[ObjectPooler.instance.GetGameObjectsOnPool("Platform").Length];
-        for (int i = 0; i < _platforms.Length; i++) {
-            _platforms[i] = ObjectPooler.instance.GetGameObjectsOnPool("Platform")[i].GetComponent<Platform>();
+    private void GetPlatforms(GameState state) {
+        if (state == GameState.Gameplay) {
+            _platforms = new Platform[ObjectPooler.instance.GetGameObjectsOnPool("Platform").Length];
+            for (int i = 0; i < _platforms.Length; i++) {
+                _platforms[i] = ObjectPooler.instance.GetGameObjectsOnPool("Platform")[i].GetComponent<Platform>();
+            }
         }
     }
 
     private void MaximizePlatformScale() {
-        for (int i = 0; i < _platformCountToMaximize; i++) {
-            _platforms[i].gameObject.transform.localScale = new Vector3(10, _platforms[i].gameObject.transform.localScale.y, _platforms[i].gameObject.transform.localScale.y);
-            _platforms[i].gameObject.transform.position = new Vector3(0, _platforms[i].gameObject.transform.position.y, _platforms[i].gameObject.transform.position.z);
+        for (int i = 0; i < _platforms.Length; i++) {
+            Debug.Log("is called");
+            if (IsPlatformOnScreen(_platforms[i])) {
+                _platforms[i].gameObject.transform.localScale = new Vector3(10, _platforms[i].gameObject.transform.localScale.y, _platforms[i].gameObject.transform.localScale.y);
+                _platforms[i].gameObject.transform.position = new Vector3(0, _platforms[i].gameObject.transform.position.y, _platforms[i].gameObject.transform.position.z);
+                _platformCountToMaximize--;
+                if (_platformCountToMaximize == 0)
+                    break;
+            }
         }
     }
 
+    private bool IsPlatformOnScreen(Platform platform) {
+        float bottomOfScreen = Camera.main.transform.position.y - 8;
+        float topOfScreen = Camera.main.transform.position.y + 8f;
+
+        if (platform.gameObject.transform.position.y > bottomOfScreen && platform.gameObject.transform.position.y < topOfScreen)
+            return true;
+        else {
+            return false;
+        }
+    }
 }
