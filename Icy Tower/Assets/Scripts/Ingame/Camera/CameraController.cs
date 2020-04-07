381325970a991887cacfdf3cf58380c0573ee9fc
burakulusahan <burakulusahan@ganover.com>
2020-04-07 13:34:15 +0300

    Control Cinemachine cameras via script

--- Icy Tower/Assets/Scripts/Ingame/Camera/CameraController.cs
+++ Icy Tower/Assets/Scripts/Ingame/Camera/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Cinemachine;
+using UnityEngine;
 
 public class CameraController : MonoBehaviour {
     [SerializeField]
@@ -7,15 +8,28 @@ public class CameraController : MonoBehaviour {
     private float _offset = 0f;
     [SerializeField]
     private float _smothSpeed = 0f;
+    [SerializeField]
+    CinemachineVirtualCamera vcam = null;
+    [SerializeField]
+    CinemachineVirtualCamera vcam2 = null;
 
     public float scrollSpeed = 0f;
 
     void LateUpdate() {
         if (_target.position.y + _offset > transform.position.y) {
-            Vector3 newPos = new Vector3(transform.position.x, _target.position.y + _offset, transform.position.z);
-            transform.position = Vector3.Lerp(transform.position, newPos, _smothSpeed);
+            vcam.Priority = 20;
+            vcam2.Priority = 10;
+            //vcam2.transform.position = new Vector3(vcam.transform.position.x, vcam.transform.position.y -1f, vcam.transform.position.z);
+            //Vector3 newPos = new Vector3(transform.position.x, _target.position.y + _offset, transform.position.z);
+            //transform.position = Vector3.Lerp(transform.position, newPos, _smothSpeed);
         } else {
-            transform.Translate(Vector3.up * Time.deltaTime * scrollSpeed);
+            vcam.Priority = 10;
+            vcam2.Priority = 20;
+            if (vcam2.transform.position.y > vcam.transform.position.y)
+                vcam2.transform.Translate(Vector3.up * Time.deltaTime * scrollSpeed);
+            else {
+                vcam2.transform.Translate(Vector3.up * Time.deltaTime * scrollSpeed * Mathf.Abs(Vector3.Distance(vcam.transform.position, vcam2.transform.position) * 5));
+            }
         }
     }
 }
