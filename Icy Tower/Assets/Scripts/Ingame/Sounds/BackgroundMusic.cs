539ba0a354465d11427d8d4973686fa473ccb725
gkaragoz <gkaragoz96@gmail.com>
2020-04-12 11:23:18 +0300

    Import and implement background musics.

---
+++ Icy Tower/Assets/Scripts/Ingame/Sounds/BackgroundMusic.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip[] _clips = null;
+
+    private AudioSource _sfx;
+
+    private void Awake() {
+        _sfx = GetComponent<AudioSource>();
+    }
+
+    private void Start() {
+        int dice = Random.Range(0, 2);
+        _sfx.clip = _clips[dice];
+        _sfx.Play();
+    }
+
+}
