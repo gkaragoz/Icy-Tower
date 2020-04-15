8db46858a5f52b401684602f1aafb310de304b2b
gkaragoz <gkaragoz96@gmail.com>
2020-04-15 22:12:12 +0300

    Create PnlMainMenu

---
+++ Icy Tower/Assets/Scripts/UI/PlayButtonTween.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayButtonTween : MonoBehaviour {
+
+    [SerializeField]
+    private LeanTweenType _type;
+
+    [SerializeField]
+    private float _time = 1f;
+
+    [SerializeField]
+    private float _scale = 1.2f;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            StartScaleAnim();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            StopScaleAnim();
+        }
+    }
+
+    public void StartScaleAnim() {
+        LeanTween.scale(gameObject, Vector3.one * _scale, _time).setEase(_type).setLoopPingPong();
+    }
+
+    public void StopScaleAnim() {
+        LeanTween.cancel(gameObject);
+
+        transform.localScale = Vector3.one;
+    }
+
+}
