2d36fd284a4c44c49eb317e081c1c779dd8895d7
gkaragoz <gkaragoz96@gmail.com>
2020-04-16 11:07:56 +0300

    Task transferred to Gürkan.

---
+++ Icy Tower/Assets/Scripts/Ingame/Cloth/ClothAccesoryData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ClothAccesoryData {
+
+    [SerializeField]
+    private ClothAccesoryTypes _clothAccesoryTypes;
+    [SerializeField]
+    private Vector2 _clothColor;
+
+    public ClothAccesoryTypes ClothAccesoryTypes { get => _clothAccesoryTypes; set => _clothAccesoryTypes = value; }
+    public Vector2 ClothColor { get => _clothColor; set => _clothColor = value; }
+}
---
+++ Icy Tower/Assets/Scripts/Ingame/Cloth/ClothChanger.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class ClothChanger : MonoBehaviour {
+
+    #region Cloth Up
+
+    [SerializeField]
+    private SkinnedMeshRenderer _skinnedMeshRendererUp = null;
+    public SkinnedMeshRenderer SkinnedMeshRendererUp { get => _skinnedMeshRendererUp; set => _skinnedMeshRendererUp = value; }
+    [SerializeField]
+    private ClothUpData[] _clothUpDatas = null;
+
+    #endregion
+
+    #region Cloth Down
+
+    [SerializeField]
+    private SkinnedMeshRenderer _skinnedMeshRendererDown = null;
+    public SkinnedMeshRenderer SkinnedMeshRendererDown { get => _skinnedMeshRendererDown; set => _skinnedMeshRendererDown = value; }
+    [SerializeField]
+    private ClothDownData[] _clothDownDatas = null;
+
+    #endregion
+
+    #region Cloth Shoe
+
+    [SerializeField]
+    private SkinnedMeshRenderer _skinnedMeshRendererShoe = null;
+    public SkinnedMeshRenderer SkinnedMeshRendererShoe { get => _skinnedMeshRendererShoe; set => _skinnedMeshRendererShoe = value; }
+    [SerializeField]
+    private ClothShoeData[] _clothShoeDatas = null;
+
+    #endregion
+
+    #region Cloth Accesory
+
+    [SerializeField]
+    private SkinnedMeshRenderer _skinnedMeshRendererAccesory = null;
+    public SkinnedMeshRenderer SkinnedMeshRendererAccesory { get => _skinnedMeshRendererAccesory; set => _skinnedMeshRendererAccesory = value; }
+    [SerializeField]
+    private ClothAccesoryData[] _clothAccesoryDatas = null;
+
+    #endregion
+
+    #region Cloth Hair
+
+    [SerializeField]
+    private SkinnedMeshRenderer _skinnedMeshRendererHair = null;
+    public SkinnedMeshRenderer SkinnedMeshRendererHair { get => _skinnedMeshRendererHair; set => _skinnedMeshRendererHair = value; }
+    [SerializeField]
+    private ClothHairData[] _clothHairDatas = null;
+
+    #endregion
+}
