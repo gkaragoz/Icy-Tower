7b3b4493665835fa30c64311755019c009741fd4
HasanGanover <hasan@ganover.com>
2020-04-17 13:45:18 +0300

    [ Configuration ] Integrated ThirdPartyLibrary and handle configuration issues

---
+++ Icy Tower/Assets/SDKs/ThirdPartyLibrary/Adverstiment/Admob/AdmobADs.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace Library.Advertisement.Admob
+{    
+     /// <summary>
+     /// AdmobADs which provide the full range of Google Admob features available to the library - banner, interstitial and rewarded
+     /// video ads for Android and IOS.
+     /// </summary>
+    
+    public class AdmobADs : MonoBehaviour
+    {
+        private string appID;
+
+        /// <summary>
+        /// Both fields represent specific platform unique App ID. We initialize mobile ads services with this ID.
+        /// </summary>
+        /// <summary>
+
+        /// <summary>
+        /// Stores unique app id for Android. When we select platform as android, we initialize our ads with this id.
+        /// </summary>
+        [Header("Google Admob Service Configuration")]
+        public string _AndroidAppID;
+
+        /// <summary>
+        /// Stores unique app id for IOS. When we select platform as ios, we initialize our ads with this id.
+        /// </summary>
+        public string _IOSAppID;
+
+        /// <summary>
+        ///  Initialize Services depending on platform with unique appID.
+        /// </summary>
+        
+        private void Start()
+        {
+            /// 
+            PlatformHandler();
+
+            /// When we publish the app, we should initialize MobileAds with appID.
+            // MobileAds.Initialize(appID);
+        }
+
+        /// <summary>
+        /// PlatformHandler sets appID by depending on the platform.
+        /// </summary>
+        public void PlatformHandler()
+        {
+            #if UNITY_ANDROID  // For Android
+                      appID = _AndroidAppID;
+            #elif UNITY_IPHONE // For IOS
+                      appID = _IOSAppID;
+            #else              // For Other Platform
+                      appID = "unexpected_platform";
+            #endif
+        }
+
+    }
+
+}
+
+
---
+++ Icy Tower/Assets/SDKs/ThirdPartyLibrary/PlayerData/Inventory.cs
@@ -0,0 +1,72 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Library.PlayerData.Inventory
+{
+	public class Inventory
+	{
+		public static List<ItemInstance> GetUserInventory()
+		{
+			int gold;
+
+			List<ItemInstance> UserInventory = new List<ItemInstance>();
+			
+			PlayFabClientAPI.GetUserInventory(new GetUserInventoryRequest(), (GetUserInventoryResult result) =>
+			{
+				foreach (ItemInstance item in result.Inventory)
+				{
+					UserInventory.Add(item);
+
+					Debug.Log(item.DisplayName);
+				}
+
+			},(error) =>
+
+			{
+				Debug.LogError("PlayFab Inventory Request: " + error.GenerateErrorReport());
+			});
+			
+			return UserInventory;
+
+		}
+
+
+		public static void ConsumeItem(string itemID, int consumeCount)
+		{
+			PlayFabClientAPI.ConsumeItem(new ConsumeItemRequest
+			{
+				ConsumeCount = consumeCount,
+				// This is a hex-string value from the GetUserInventory result
+
+				ItemInstanceId = itemID
+
+			}, (result) =>
+
+			{
+				Debug.Log("Item Consumed");
+
+			}, (error) =>
+
+			{
+				Debug.Log(error.GenerateErrorReport()) ;
+			});
+
+		}
+
+
+		public static void ConsumeItems(List <string> consumedItemList)
+		{
+			foreach (string item in consumedItemList)
+			{
+				ConsumeItem(item,1);
+			}
+			
+		}
+
+	}
+
+}
+
