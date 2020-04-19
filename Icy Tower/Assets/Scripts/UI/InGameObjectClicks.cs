e69fc18a19f0ad18ed3da4f3c4d32bbb43057113
gkaragoz <gkaragoz96@gmail.com>
2020-04-19 17:25:14 +0300

    Create WorldSpaceUI scene.

---
+++ Icy Tower/Assets/Scripts/UI/InGameObjectClicks.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class InGameObjectClicks : MonoBehaviour {
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f)) {
+                if (hit.transform != null) {
+                    IngameObjectUI ui = hit.transform.GetComponent<IngameObjectUI>();
+
+                    if (ui != null) {
+                        ui.OnClick();
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ Icy Tower/Assets/Scripts/UI/IngameObjectUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class IngameObjectUI : MonoBehaviour {
+
+    public UnityEvent unityEvent;
+
+    public void OnClick() {
+        unityEvent.Invoke();
+    }
+
+}
