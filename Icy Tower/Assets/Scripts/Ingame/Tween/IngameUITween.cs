7a41ddc9d2681ffca6775033744fa70c4c844bc6
gkaragoz <gkaragoz96@gmail.com>
2020-04-20 14:57:38 +0300

    Resize and repositions of Ingame UIs.

--- Icy Tower/Assets/Scripts/Ingame/Tween/IngameUITween.cs
+++ Icy Tower/Assets/Scripts/Ingame/Tween/IngameUITween.cs
@@ -6,10 +6,13 @@ public class IngameUITween : MonoBehaviour {
     public float idleRotationValue = 10f;
     public float idleScale = 0.2f;
 
+    private Vector3 _initialScale = Vector3.zero;
+
     private void Start() {
+        _initialScale = transform.localScale;
         transform.localScale = Vector3.zero;
 
-        LeanTween.scale(gameObject, Vector3.one * 0.1f, 0.2f).setDelay(Random.Range(1f, 1.45f)).setEaseInBounce().setIgnoreTimeScale(true).setOnComplete(PlayHello);
+        LeanTween.scale(gameObject, _initialScale, 0.2f).setDelay(Random.Range(1f, 1.45f)).setEaseInBounce().setIgnoreTimeScale(true).setOnComplete(PlayHello);
     }
 
     private void PlayHello() {
