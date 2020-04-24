28cb1785787fd14c313a2f611b893e16cdf71b69
gkaragoz <gkaragoz96@gmail.com>
2020-04-24 13:46:23 +0300

    Remove MarketCharacter and try to implement replace position of real character between Wardrobe and Gameplay positions.

--- Icy Tower/Assets/Scripts/Ingame/Cloth/ClothManager.cs
+++ Icy Tower/Assets/Scripts/Ingame/Cloth/ClothManager.cs
@@ -6,14 +6,12 @@ public class ClothManager:MonoBehaviour
 {
    public ClothS_SO clothData;
    public Renderer _myRenderer;
-  
- 
+
+
     public void SetColor(int value)
     {
         _myRenderer.material = clothData.ObjectMaterial;
         _myRenderer.material.SetVector("OFFSETREF", clothData.ColorOfsets[value].colorOfset);
-       // _myRenderer.material.SetTextureOffset("_Vector2_B985E7DD", clothData.ColorOfsets[value].colorOfset);
-        Debug.Log("My Color is : "+ clothData.ColorOfsets[value].colorName);
     }
 
 }
