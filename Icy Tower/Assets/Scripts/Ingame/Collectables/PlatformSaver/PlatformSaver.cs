1aa515f9ac4fecf96b216c138c0e2b9c25b47ca9
YesilyurtGurkan <gurkan@ganover.com>
2020-04-29 13:53:57 +0300

    Fixed PlatformSaver problem

--- Icy Tower/Assets/Scripts/Ingame/Collectables/PlatformSaver/PlatformSaver.cs
+++ Icy Tower/Assets/Scripts/Ingame/Collectables/PlatformSaver/PlatformSaver.cs
@@ -28,8 +28,11 @@ public class PlatformSaver : MonoBehaviour, IHaveSingleSound {
         for (int i = 0; i < _platformCountToMaximize; i++) {
             Platform platformToMaximize = PlatformManager.instance.GetSpawnedPlatformAtFloor(Account.instance.GetCurrentScore() + i);
             GameObject childPlatform = platformToMaximize.Types[PlatformManager.instance.PlatformTypeIndex];
-            childPlatform.transform.localPosition = Vector3.zero;
-            childPlatform.transform.localScale = new Vector3(childPlatform.transform.localScale.x, childPlatform.transform.localScale.y, 10f);
+            // childPlatform.transform.localPosition = Vector3.zero;
+            childPlatform.transform.localPosition = new Vector3(3.7f,0,0);
+           // childPlatform.transform.localScale = new Vector3(childPlatform.transform.localScale.x, childPlatform.transform.localScale.y, 10f);
+            childPlatform.transform.localScale = new Vector3(1, 1, 1.7f);
+            childPlatform.GetComponentInParent<MovingPlatform>().StopMovement();
         }
     }
 
