3bf2cd12fd20572ced16ce2bad5c8db70271a645
gkaragoz <gkaragoz96@gmail.com>
2020-05-03 16:26:08 +0300

    Bug fix loading screen, loading bar.

--- Icy Tower/Assets/_TowerRacer/Scripts/Services/Strings.cs
+++ Icy Tower/Assets/_TowerRacer/Scripts/Services/Strings.cs
@@ -1,5 +1,20 @@
 ﻿public class Strings {
-
+    
+    // ID
     public static string LEADERBOARD_01 = "Leaderboard01";
+
+    // Messages to users
+    public static string ON_GAME_STARTING = "Loading...";
+    public static string ON_GPGS_ACCOUNT_INITIALIZATION_BEGIN_MESSAGE = "Trying to login into Google Play Games...";
+    public static string ON_GPGS_ACCOUNT_INITIALIZATION_SUCCESS_MESSAGE = "Succesfully login into Google Play Games";
+    public static string ON_GPGS_ACCOUNT_INITIALIZATION_FAILED_MESSAGE = "Failed on Google Play Games login!";
+
+    public static string ON_PLAYFAB_ACCOUNT_INITIALIZATION_BEGIN_MESSAGE = "Trying to login into server...";
+    public static string ON_PLAYFAB_ACCOUNT_INITIALIZATION_SUCCESS_MESSAGE = "Successfully connected to server";
+    public static string ON_PLAYFAB_ACCOUNT_INITIALIZATION_FAILED_MESSAGE = "Failed on connect to server!";
+
+    public static string ON_ACCOUNT_LOADED_MESSAGE = "Account initializing...";
+    public static string ON_POOL_LOADED_MESSAGE = "Scene preparing...";
+    public static string ON_SCENE_READY_MESSAGE = "Scene successfully prepared...";
 
 }
--- Icy Tower/Assets/_TowerRacer/Scripts/UI/LoadingBar.cs
+++ Icy Tower/Assets/_TowerRacer/Scripts/UI/LoadingBar.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,29 +8,123 @@ public class LoadingBar : MonoBehaviour {
 
     [SerializeField]
     private LoadManager _loadManager = null;
+    [SerializeField]
+    private TextMeshProUGUI _txtMessage = null;
 
     private Image _image;
-    private int _totalProgress = 0;
-    private int _incrementValue = 0;
+    private float _totalProgress = 0;
+    private int _completedActionsCount = 0;
 
     private List<Action> _progressActions = new List<Action>();
 
     private void Awake() {
         _image = GetComponent<Image>();
 
-        _loadManager.OnAccountLoaded += OnTotalProgressChanged;
-        _loadManager.OnPoolLoaded += OnTotalProgressChanged;
-        _loadManager.OnSceneReady += OnTotalProgressChanged;
+        _loadManager.OnGPGSAccountInitializationBegin += OnGPGSAccountInitializationBegin;
+        _loadManager.OnGPGSAccountInitializationSuccess += OnGPGSAccountInitializationSuccess;
+        _loadManager.OnGPGSAccountInitiailzationFailed += OnGPGSAccountInitiailzationFailed;
+
+        _loadManager.OnPlayFabAccountInitializationBegin += OnPlayFabAccountInitializationBegin;
+        _loadManager.OnPlayFabAccountInitializationSuccess += OnPlayFabAccountInitializationSuccess;
+        _loadManager.OnPlayFabAccountInitiailzationFailed += OnPlayFabAccountInitiailzationFailed;
+
+        _loadManager.OnAccountLoaded += OnAccountLoaded;
+        _loadManager.OnPoolLoaded += OnPoolLoaded;
+        _loadManager.OnSceneReady += OnSceneReady;
+
+        _progressActions.Add(_loadManager.OnGPGSAccountInitializationBegin);
+        _progressActions.Add(_loadManager.OnGPGSAccountInitializationSuccess);
+        _progressActions.Add(_loadManager.OnGPGSAccountInitiailzationFailed);
+
+        _progressActions.Add(_loadManager.OnPlayFabAccountInitializationBegin);
+        _progressActions.Add(_loadManager.OnPlayFabAccountInitializationSuccess);
+        _progressActions.Add(_loadManager.OnPlayFabAccountInitiailzationFailed);
 
         _progressActions.Add(_loadManager.OnAccountLoaded);
         _progressActions.Add(_loadManager.OnPoolLoaded);
         _progressActions.Add(_loadManager.OnSceneReady);
 
-        _incrementValue = 100 / _progressActions.Count;
+        _totalProgress = 0;
+        _completedActionsCount = 0;
+
+        SetMessage(Strings.ON_GAME_STARTING);
+
+        UpdateFillBar();
+    }
+
+    private void OnAccountLoaded() {
+        SetMessage(Strings.ON_ACCOUNT_LOADED_MESSAGE);
+
+        OnTotalProgressChanged();
+    }
+
+    private void OnPoolLoaded() {
+        SetMessage(Strings.ON_POOL_LOADED_MESSAGE);
+
+        OnTotalProgressChanged();
+    }
+
+    private void OnSceneReady() {
+        SetMessage(Strings.ON_SCENE_READY_MESSAGE);
+
+        OnTotalProgressChanged();
+    }
+
+    private void OnGPGSAccountInitializationBegin() {
+        _progressActions.Remove(_loadManager.OnPlayFabAccountInitializationBegin);
+        _progressActions.Remove(_loadManager.OnPlayFabAccountInitializationSuccess);
+        _progressActions.Remove(_loadManager.OnPlayFabAccountInitiailzationFailed);
+
+        SetMessage(Strings.ON_GPGS_ACCOUNT_INITIALIZATION_BEGIN_MESSAGE);
+
+        OnTotalProgressChanged();
+    }
+
+    private void OnGPGSAccountInitializationSuccess() {
+        SetMessage(Strings.ON_GPGS_ACCOUNT_INITIALIZATION_SUCCESS_MESSAGE);
+
+        OnTotalProgressChanged();
+    }
+
+    private void OnGPGSAccountInitiailzationFailed() {
+        _progressActions.Remove(_loadManager.OnGPGSAccountInitializationBegin);
+        _progressActions.Remove(_loadManager.OnGPGSAccountInitializationSuccess);
+        _progressActions.Remove(_loadManager.OnGPGSAccountInitiailzationFailed);
+
+        SetMessage(Strings.ON_GPGS_ACCOUNT_INITIALIZATION_FAILED_MESSAGE);
+
+        OnTotalProgressChanged();
+    }
+
+    private void OnPlayFabAccountInitializationBegin() {
+        _progressActions.Remove(_loadManager.OnGPGSAccountInitializationBegin);
+        _progressActions.Remove(_loadManager.OnGPGSAccountInitializationSuccess);
+        _progressActions.Remove(_loadManager.OnGPGSAccountInitiailzationFailed);
+
+        SetMessage(Strings.ON_PLAYFAB_ACCOUNT_INITIALIZATION_BEGIN_MESSAGE);
+
+        OnTotalProgressChanged();
+    }
+
+    private void OnPlayFabAccountInitializationSuccess() {
+        SetMessage(Strings.ON_PLAYFAB_ACCOUNT_INITIALIZATION_SUCCESS_MESSAGE);
+
+        OnTotalProgressChanged();
+    }
+
+    private void OnPlayFabAccountInitiailzationFailed() {
+        _progressActions.Remove(_loadManager.OnPlayFabAccountInitializationBegin);
+        _progressActions.Remove(_loadManager.OnPlayFabAccountInitializationSuccess);
+        _progressActions.Remove(_loadManager.OnPlayFabAccountInitiailzationFailed);
+
+        SetMessage(Strings.ON_PLAYFAB_ACCOUNT_INITIALIZATION_FAILED_MESSAGE);
+
+        OnTotalProgressChanged();
     }
 
     private void OnTotalProgressChanged() {
-        _totalProgress += _incrementValue;
+        _completedActionsCount++;
+        _totalProgress = 20 * _completedActionsCount;
 
         if (_totalProgress >= 99) {
             _totalProgress = 100;
@@ -44,6 +139,10 @@ public class LoadingBar : MonoBehaviour {
 
     private void UpdateFillBar() {
         _image.fillAmount = _totalProgress / 100;
+    }
+
+    private void SetMessage(string message) {
+        _txtMessage.text = message;
     }
 
 }
