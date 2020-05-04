85138cf7c21f28f1e2190ae78a63d96a1421eb01
gkaragoz <gkaragoz96@gmail.com>
2020-05-04 22:15:34 +0300

    Setup notifications on application quit and on application pause states.

--- Icy Tower/Assets/_TowerRacer/Scripts/Services/NotificationExample.cs
+++ Icy Tower/Assets/_TowerRacer/Scripts/Services/NotificationExample.cs
@@ -1,8 +1,7 @@
 ﻿using Library.PushNotification;
 using UnityEngine;
 
-public class NotificationExample : MonoBehaviour
-{
+public class NotificationExample : MonoBehaviour {
     // Notification Title
     private const string Notification_Title = "Race Tower";
 
@@ -14,19 +13,35 @@ public class NotificationExample : MonoBehaviour
 
     private AndroidNotifications _notifications;
 
-    void Start()
-    {
+    private void Start() {
         _notifications = new AndroidNotifications();
+    }
 
+    private void Setup() {
         _notifications.ResetAll();
 
         _notifications.CreateNotificationChannel();
 
         // 1 HOUR
         _notifications.AddNotification(Notification_Title, Notification_Text, 1, Notification_LargeIconID);
+        // 3 HOUR
+        _notifications.AddNotification(Notification_Title, Notification_Text, 3, Notification_LargeIconID);
         // 7 HOUR
         _notifications.AddNotification(Notification_Title, Notification_Text, 7, Notification_LargeIconID);
         // 1 DAY
         _notifications.AddNotification(Notification_Title, Notification_Text, 24, Notification_LargeIconID);
     }
+
+    private void OnApplicationQuit() {
+        Setup();
+    }
+
+    private void OnApplicationPause(bool pause) {
+        if (pause) {
+            Setup();
+        } else {
+            _notifications.ResetAll();
+        }
+    }
+
 }
