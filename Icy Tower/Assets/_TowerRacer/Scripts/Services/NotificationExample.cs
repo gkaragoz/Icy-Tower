d49b35a360ce2b5592f5de78c46e75e2ce7c2f8f
gkaragoz <gkaragoz96@gmail.com>
2020-05-04 22:03:24 +0300

    Implement notification feature. 1 hour, 1 day, 7 days.

---
+++ Icy Tower/Assets/_TowerRacer/Scripts/Services/NotificationExample.cs
@@ -0,0 +1,32 @@
+﻿using Library.PushNotification;
+using UnityEngine;
+
+public class NotificationExample : MonoBehaviour
+{
+    // Notification Title
+    private const string Notification_Title = "Race Tower";
+
+    // Notification Text
+    private const string Notification_Text = "Hey! We missed you!";
+
+    // Notification Icon
+    private const string Notification_LargeIconID = "icon_01";
+
+    private AndroidNotifications _notifications;
+
+    void Start()
+    {
+        _notifications = new AndroidNotifications();
+
+        _notifications.ResetAll();
+
+        _notifications.CreateNotificationChannel();
+
+        // 1 HOUR
+        _notifications.AddNotification(Notification_Title, Notification_Text, 1, Notification_LargeIconID);
+        // 7 HOUR
+        _notifications.AddNotification(Notification_Title, Notification_Text, 7, Notification_LargeIconID);
+        // 1 DAY
+        _notifications.AddNotification(Notification_Title, Notification_Text, 24, Notification_LargeIconID);
+    }
+}
