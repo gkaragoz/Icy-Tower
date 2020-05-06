b8e2325aab77a4d4d38a6577b49d3ce2f46c0b82
YesilyurtGurkan <gurkan@ganover.com>
2020-05-06 14:07:57 +0300

    Add watch ads to revive panel

---
+++ Icy Tower/Assets/_TowerRacer/Scripts/UI/WatchAdsGameOver.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WatchAdsGameOver : MonoBehaviour
+{
+    [SerializeField]
+    GameObject gameOverMenuPanel;
+
+
+
+    [SerializeField]
+    Slider _adsSlider;
+
+    bool timerStart;
+    float sliderValue = 0;
+
+
+    private void Start()
+    {
+        StartTimerRevive();
+    }
+
+
+    public void StartTimerRevive()
+    {
+        sliderValue = 0;
+        timerStart = true;
+        StartCoroutine(SliderTimer());
+    }
+
+    IEnumerator SliderTimer()
+    {
+        while (timerStart)
+        {
+            sliderValue += Time.deltaTime/2;
+            _adsSlider.value = sliderValue;
+            if (sliderValue>=1)
+            {
+
+                transform.gameObject.SetActive(false);
+                gameOverMenuPanel.SetActive(true);
+                timerStart = false;
+                    
+            }
+            yield return null;
+        }
+    }
+
+
+}
